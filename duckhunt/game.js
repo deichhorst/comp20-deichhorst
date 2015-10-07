57351b4d31d1ce6377a9b895b0cf656d679dc0e8
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-07 13:00:36 -0400

    added 2 birds to be drawn on canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,18 +1,23 @@
-// Replace this with your code...
 function init () {
-	var canvas = document.getElementById('game_canvas');
-  	var ctx = canvas.getContext('2d');
-
-	// Draw slice
-	//ctx.drawImage(document.getElementById('source'),
-    //33, 71, 104, 124, 21, 20, 87, 104);
-
-  	// Draw frame
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext('2d');
+    // get background
   	var background = new Image();
   	background.src = 'duckhunt-background.gif';
+    //get birds
+    var bird1 = new Image();
+    bird1.src = 'duckhunt_various_sheet.png';
+    var bird2 = new Image();
+    bird2.src = 'duckhunt_various_sheet.png';
   	
   	background.onload = function () {
   		ctx.drawImage(background,0,0,256,240,0,0,512,480);
   	}
- 	
+    
+    bird1.onload = function () {
+      ctx.drawImage(bird1,0,110,35,35,145,65,80,80);
+    }
+    bird2.onload = function () {
+      ctx.drawImage(bird1,130,110,35,35,285,95,80,80);
+    }
 }
