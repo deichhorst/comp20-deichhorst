692e17af0bf9fd3a2b851e9da9ee74856a3162b9
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-07 12:36:21 -0400

    added background drawing

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,18 @@
 // Replace this with your code...
+function init () {
+	var canvas = document.getElementById('game_canvas');
+  	var ctx = canvas.getContext('2d');
+
+	// Draw slice
+	//ctx.drawImage(document.getElementById('source'),
+    //33, 71, 104, 124, 21, 20, 87, 104);
+
+  	// Draw frame
+  	var background = new Image();
+  	background.src = 'duckhunt-background.gif';
+  	
+  	background.onload = function () {
+  		ctx.drawImage(background,0,0,256,240,0,0,512,480);
+  	}
+ 	
+}
