6ec7f99256794cec5282a00d2b747c974f78ba9e
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-07 13:32:29 -0400

    changed bird2 position

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -5,19 +5,15 @@ function init () {
   	var background = new Image();
   	background.src = 'duckhunt-background.gif';
     //get birds
-    var bird1 = new Image();
-    bird1.src = 'duckhunt_various_sheet.png';
-    var bird2 = new Image();
-    bird2.src = 'duckhunt_various_sheet.png';
-  	
+    var birds = new Image();
+    birds.src = 'duckhunt_various_sheet.png';
+    
   	background.onload = function () {
   		ctx.drawImage(background,0,0,256,240,0,0,512,480);
   	}
     
-    bird1.onload = function () {
-      ctx.drawImage(bird1,0,110,35,35,145,65,80,80);
-    }
-    bird2.onload = function () {
-      ctx.drawImage(bird1,130,110,35,35,285,95,80,80);
+    birds.onload = function () {
+      ctx.drawImage(birds,0,110,35,35,145,65,80,80);
+      ctx.drawImage(birds,130,110,35,35,285,95,80,80);
     }
 }
