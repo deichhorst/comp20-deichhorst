2e89ccff3f242ed62632f644b6b077920c3df4e5
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-16 22:18:45 -0400

    modified javascript

--- messages/lab.js
+++ messages/lab.js
@@ -1,14 +1,5 @@
 var xhr = new XMLHttpRequest();
 var json;
 
-nokokj;
-
-xhr.onreadystatechange = function () {
-	if(xhr.readyState === 4 && xhrhttp.status === 200)//good to go
-		json = JSON.parse(xhr.responseText);
-		console.log(json);
-	}
-};
-
 xhr.open('GET', 'data.json', true);
 xhr.send();
