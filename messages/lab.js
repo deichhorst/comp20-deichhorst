46816aba9259dbd69133db685abaf7660c435716
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-16 22:53:06 -0400

    newest function updates

--- messages/lab.js
+++ messages/lab.js
@@ -2,6 +2,12 @@ function parse(){
 	var xhr = new XMLHttpRequest();
 	var json;
 
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === 4 && xhr.status === 200) {
+			console.log('You da man');
+		}
+	};
+
 	xhr.open('GET', 'data.json', true);
 	xhr.send();
 
