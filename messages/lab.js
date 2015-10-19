3930f319abb06e4ee55b8e1c75ab7b35233de9a4
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-19 11:52:28 -0400

    new json loop

--- messages/lab.js
+++ messages/lab.js
@@ -1,19 +1,14 @@
 function parse(){
 	var xhr = new XMLHttpRequest();
 	var json;
-
-	//xhr.onreadystatechange = function() {
-	//	if(xhr.readyState === 4 && xhr.status === 200) {
-			json = JSON.parse(xhr.responseText);
-	//	}
-	//};
-
+	json = JSON.parse(xhr.responseText);
+	
 	xhr.open('GET', 'data.json', true);
 	xhr.send();
 
 	elem = document.getElementById("messages");
 	
-	for (count = 0; count < 2; count++) {
+	for (count = 0; count < json.length; count++) {
 			elem.innerHTML += "<p> <h2>" + json[count]["content"] + "</h2>" + json[count]["username"] + "</p>";
 	}
 }
