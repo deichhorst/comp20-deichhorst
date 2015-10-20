0154f8ab5b66f316ef5d608ffb1715f39aced8dc
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-20 13:37:57 -0400

    incorporating messages div

--- messages/lab.js
+++ messages/lab.js
@@ -1,20 +1,20 @@
 function parse(){
 	var xhr = new XMLHttpRequest();
 	var json;
-	console.log('assigned variables');
 	
 	xhr.open('GET', 'data.json', true);
 	xhr.send();
 
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
-        	console.log('did stuff');
-        	json = JSON.parse(xhr.responseText);
+	        json = JSON.parse(xhr.responseText);
         	console.log(json.length);
     	}
     };
-	//elem = document.getElementById("messages");
-	//for (count = 0; count < json.length; count++) {
-	//elem.innerHTML += "<p> <h2>" + json[count]["content"] + "</h2>" + json[count]["username"] + "</p>";
-	//}
+	
+	elem = document.getElementById("messages");
+
+	elem.innerHTML += "<p> <h2>" + json[0]["content"] + "</h2>" + json[0]["username"] + "</p>";
+	
+	elem.innerHTML += "<p> <h2>" + json[1]["content"] + "</h2>" + json[1]["username"] + "</p>";
 }
