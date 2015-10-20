b2065864f735d7dfebd7c95aa6f13328481f4515
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-20 13:53:54 -0400

    finalizing messages

--- messages/lab.js
+++ messages/lab.js
@@ -8,13 +8,12 @@ function parse(){
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
 	        json = JSON.parse(xhr.responseText);
-        	console.log(json.length);
         	elem = document.getElementById("messages");
 
-			elem.innerHTML += "<p> <h2>" + json[0]["content"] + 
-			"</h2>" + json[0]["username"] + "</p>";
-			elem.innerHTML += "<p> <h2>" + json[1]["content"] + 
-			"</h2>" + json[1]["username"] + "</p>";
+        	for (count = 0; count < json.length; count++) {
+				elem.innerHTML += "<p> <h2>" + json[count]["content"] + 
+				"</h2>" + json[count]["username"] + "</p>";
+    		}
     	}
     };
 	
