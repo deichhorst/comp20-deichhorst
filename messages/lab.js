40e05ac57e02e174aa1317f7f4b3ed0be728753f
deichhorst <dexter.eichhorst@tufts.edu>
2015-10-20 11:08:36 -0400

    debugging function

--- messages/lab.js
+++ messages/lab.js
@@ -1,15 +1,19 @@
 function parse(){
 	var xhr = new XMLHttpRequest();
-	var json;
+	var json = 4;
+	console.log('assigned variables');
 	
 	xhr.open('GET', 'data.json', true);
 	xhr.send();
 
-	json = JSON.parse(xhr.responseText);
-	
-	elem = document.getElementById("messages");
-	
-	for (count = 0; count < json.length; count++) {
-			elem.innerHTML += "<p> <h2>" + json[count]["content"] + "</h2>" + json[count]["username"] + "</p>";
-	}
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4){
+        	console.log('did stuff');
+    	}
+    };
+	//json = JSON.parse(xhr.responseText);
+	//elem = document.getElementById("messages");
+	//for (count = 0; count < json.length; count++) {
+	//		elem.innerHTML += "<p> <h2>" + json[count]["content"] + "</h2>" + json[count]["username"] + "</p>";
+	//}
 }
